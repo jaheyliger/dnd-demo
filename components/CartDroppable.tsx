aad09927d146b23409ac350bd6326dc365b676d1
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-11-04 01:20:49 -0400

    added draggable

--- components/CartDroppable.tsx
+++ components/CartDroppable.tsx
@@ -1,3 +1,5 @@
+'use-client';
+
 import { useDroppable } from '@dnd-kit/core';
 import { FC } from 'react';
 
@@ -10,21 +12,21 @@ const CartDroppable: FC<ICartDroppable> = ({ items }) => {
 		id: 'cart-droppable'
 	});
 
-    return (
-			<ul
-				ref={setNodeRef}
-				className='w-4/5  border flex rounded-xl shadow-lg shadow-gray-500 space-x-2 p-4'
-			>
-				{items.map((item, index) => (
-					<div
-						key={`${item}-${index}`}
-						className='rounded-xl border cursor-grab active:cursor-grabbing h-min-content border-slate-500 px-3 py-2 text-lg transition duration-300'
-					>
-						{item}
-					</div>
-				))}
-			</ul>
-		);
+	return (
+		<ul
+			ref={setNodeRef}
+			className='w-4/5  border flex rounded-xl shadow-lg shadow-gray-500 space-x-2 p-4'
+		>
+			{items.map((item, index) => (
+				<div
+					key={`${item}-${index}`}
+					className='rounded-xl border cursor-grab active:cursor-grabbing h-min-content border-slate-500 px-3 py-2 text-lg transition duration-300'
+				>
+					{item}
+				</div>
+			))}
+		</ul>
+	);
 };
 
 export default CartDroppable;
--- components/FruitDraggable.tsx
+++ components/FruitDraggable.tsx
@@ -1,3 +1,5 @@
+"use-client"
+
 import { useDraggable } from '@dnd-kit/core';
 import { FC } from 'react';
 import {CSS} from '@dnd-kit/utilities'
@@ -8,14 +10,14 @@ interface IFruitDraggable {
 
 const FruitDraggable: FC<IFruitDraggable> = ({ children }) => {
 	const { attributes, listeners, setNodeRef, transform } = useDraggable({
-        id: children,
-        data: {title: children}
+		id: children,
+		data: { title: children }
 	});
 
 	return (
 		<div
 			ref={setNodeRef}
-			className='rounded-xl border cursor-grab active:cursor-grabbing border-slate-500 hover:bg-slate-500 px-3 py-2 text-lg transition duration-300'
+			className='rounded-xl border cursor-grab active:cursor-grabbing border-slate-500  px-3 py-2 text-lg transition duration-300'
 			style={{ transform: CSS.Translate.toString(transform) }}
 			{...attributes}
 			{...listeners}
